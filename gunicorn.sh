483170474f2f1993e04dc46e3b8156a872884369
root <root@dpngnxtes02d.(none)>
2016-12-13 11:40:22 +0300

    Run with gunicorn

---
+++ gunicorn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ 
+NAME="EQL"
+FLASKDIR=/EQL
+SOCKFILE=/EQL/sock
+USER=root
+GROUP=root
+NUM_WORKERS=10
+ 
+echo "Starting $NAME"
+ 
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+ 
+# Start your gunicorn
+exec gunicorn main:app -b 127.0.0.1:5000 \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --bind=unix:$SOCKFILE
